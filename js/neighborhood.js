48a231801cdf7f4f32857d3839fc9f99deb3e0a2
Jake Siddall <jacobsiddall@gmail.com>
2016-05-31 10:07:43 -0500

    added address to the map; zoomed map in a bit

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   map = new GMaps({
     el: '#map',
     lat: 36.1532540,
-    lng: -86.7922290
+    lng: -86.7922290,
+    zoom: 17
   });
   map.addMarker({
     lat: 36.1532540,
     lng: -86.7922290,
     title: 'The Cadence',
     infoWindow: {
-      content: '<p>The Cadence</p>'
+      content: '<p><stong>The Cadence</stong></p><p>1600 McGavock St</p><p>Nashville, TN 37203</p>'
     }
   });
 });
